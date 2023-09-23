f3895328bbbd37e0f45364ff75e15ccd071a933d
Haos Games <haosgames@protonmail.com>
2023-09-23 22:54:20 +0200

    more pretty errors

--- src/api/util.rs
+++ src/api/util.rs
@@ -3,9 +3,9 @@ use reqwest::StatusCode;
 
 pub fn map_any_err_and_code(e: anyhow::Error) -> (StatusCode, String) {
     debug!("Error: {:#}", e);
-    (StatusCode::INTERNAL_SERVER_ERROR, format!("{:#}", e))
+    (StatusCode::INTERNAL_SERVER_ERROR, format!("{}", e))
 }
 pub fn map_any_err(e: anyhow::Error) -> String {
     debug!("Error: {:#}", e);
-    format!("{:#}", e)
+    format!("{}", e)
 }
